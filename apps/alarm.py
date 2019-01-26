5b7260faab01da941d2032654e2a6b775bacba46
Janos Molnar <janosmolnar@janosmolnar.t.hu>
2019-01-26 17:12:23 +0100

    Add progressive alarm, not tested yet

--- apps/alarm.py
+++ apps/alarm.py
@@ -13,28 +13,34 @@ from dateutil.relativedelta import relativedelta
 class AlarmService(hass.Hass):
 
     def initialize(self):
+
         self.log("Started Alarm service")
         self.listen_state(self.check_time, "sensor.time")
 
-        self.alarm_time = self.get_state(self.args['alarm_time'])
-        self.listen_state(self.set_alarm, self.args['alarm_time'])
+        self.alarm_minutes = 30
+        self.alarm_on_minutes = 0
 
-        #self.toggle_light()
+        self.current_brightness = 0
+        self.target_brightness = 255
+        self.brightness_step = 255 / float(self.alarm_minutes)
+
+        self.wakeup_time = self.get_state(self.args['wakeup_time'])
+        self.listen_state(self.set_alarm, self.args['wakeup_time'])
 
     def set_alarm(self, entity, attribute, old, new, kwargs):
         try:
-            self.alarm_time = parse(new).time()
-            self.log('Alarm set to: {}'.format(self.alarm_time))
+            self.wakeup_time = parse(new)
+            self.alarm_start = self.wakeup_time - relativedelta(minutes=self.alarm_minutes)
         except ValueError:
             self.log('Alarm time is invalid!')
 
     def check_time(self, entity, attribute, old, new, kwargs):
-        current_time = parse(new).time()
-        #self.log('Current time: {}, Alarm time: {}'.format(current_time, self.alarm_time))
+        current_time = parse(new)
 
-        if current_time == self.alarm_time:
+        if current_time >= self.alarm_start:
             self.log('Starting wake up')
             self.start_alarm()
+            self.current_brightness += self.brightness_step
 
     def start_alarm(self):
-        self.turn_on('light.jani_s_room', brightness=225, color_temp=1)
+        self.turn_on('light.jani_s_room', brightness=self.current_brightness, color_temp=1)
