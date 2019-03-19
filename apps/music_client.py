36c4c9a035e8c862367d40b75d963fcacdddcd27
Janos Molnar <janosmolnar@janosmolnar.local>
2019-03-19 19:54:28 +0100

    Fix bugs, restructure code

--- apps/music_client.py
+++ apps/music_client.py
@@ -10,29 +10,21 @@ class MusicClient(object):
         self.headers = {
             'Content-Type': 'application/json',
         }
-        self.params = {}
-        self.method = ''
 
-    def _send_command(self):
-        self.data = {"jsonrpc": "2.0", "id": 1, "method": self.method, "params": self.params}
-        print(self.data)
+    def _send_command(self, method='', params={}):
+        self.data = {"jsonrpc": "2.0", "id": 1, "method": method, "params": params}
         response = requests.post(self.url, headers=self.headers, data=json.dumps(self.data))
         return response.json()
 
     def start(self):
-        self.method = 'core.playback.play'
-        return self._send_command()
+        return self._send_command('core.playback.play')
 
     def stop(self):
-        self.method = 'core.playback.stop'
-        return self._send_command()
+        return self._send_command('core.playback.stop')
 
     def get_volume(self):
-        self.method = 'core.mixer.get_volume'
-        response = self._send_command()
+        response = self._send_command('core.mixer.get_volume')
         return response['result']
 
     def set_volume(self, volume):
-        self.method = 'core.mixer.get_volume'
-        self.params = {'volume': volume}
-        return self._send_command()
+        return self._send_command('core.mixer.set_volume', {'volume': volume})
