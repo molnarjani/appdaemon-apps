58d5cda6ccbc3a35be8a0d97b6a8108bb1f7012e
Janos Molnar <janosmolnar@janosmolnar.local>
2019-03-19 19:40:09 +0100

    Separate music client from alarm

--- apps/alarm.py
+++ apps/alarm.py
@@ -10,32 +10,7 @@ import requests
 from dateutil.parser import parse
 from dateutil.relativedelta import relativedelta
 
-class MusicService(object):
-    def __init__(self, url):
-        self.url = url
-        self.headers = {
-            'Content-Type': 'application/json',
-        }
-        self.params = {}
-        self.method = ''
-        self.data = {"jsonrpc": "2.0", "id": 1, "method": self.method, "params": self.params}
 
-    def _send_command(self):
-        response = requests.post('http://127.0.0.1:6680/mopidy/rpc', headers=self.headers, data=self.data)
-        return response.json()
-
-    def start(self):
-        self.method = 'core.playback.play'
-        return self._send_command()
-
-    def stop(self):
-        self.method = 'core.playback.stop'
-        return self._send_command()
-
-    def get_volume(self):
-        self.method = 'core.mixer.get_volume'
-        response = self._send_command()
-        return response
 
 
 class AlarmService(hass.Hass):
