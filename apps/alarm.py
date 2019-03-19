8c9cc8129deddcda4003488bcc67e7540ac5aea3
Janos Molnar <janosmolnar@janosmolnar.local>
2019-03-19 19:37:25 +0100

    WIP

--- apps/alarm.py
+++ apps/alarm.py
@@ -1,29 +1,42 @@
 import appdaemon.plugins.hass.hassapi as hass
 
-#
 # Alarm App
 #
 # Args:
-#   - alarm_time: time you want to wake up at
-#   - light_id: light to trigger
-#
+#   - wakeup_time: time you want to wake up at
 import json
+import requests
 
 from dateutil.parser import parse
 from dateutil.relativedelta import relativedelta
 
 class MusicService(object):
-    """
-    In [88]: import requests
-    ...:
-    ...: headers = {
-        ...:     'Content-Type': 'application/json',
-                                 ...: }
-    ...:
-    ...: data = '{"jsonrpc": "2.0", "id": 1, "method": "core.mixer.get_volume"}'
-    ...:
-    ...: response = requests.post('http://127.0.0.1:6680/mopidy/rpc', headers=headers, data=data)
-    """
+    def __init__(self, url):
+        self.url = url
+        self.headers = {
+            'Content-Type': 'application/json',
+        }
+        self.params = {}
+        self.method = ''
+        self.data = {"jsonrpc": "2.0", "id": 1, "method": self.method, "params": self.params}
+
+    def _send_command(self):
+        response = requests.post('http://127.0.0.1:6680/mopidy/rpc', headers=self.headers, data=self.data)
+        return response.json()
+
+    def start(self):
+        self.method = 'core.playback.play'
+        return self._send_command()
+
+    def stop(self):
+        self.method = 'core.playback.stop'
+        return self._send_command()
+
+    def get_volume(self):
+        self.method = 'core.mixer.get_volume'
+        response = self._send_command()
+        return response
+
 
 class AlarmService(hass.Hass):
 
