7a57e753f73377ba555f17d9a6ddd27c64a64f67
Janos Molnar <janosmolnar1001@gmail.com>
2019-05-31 21:06:28 +0200

    WIP

--- apps/cron.py
+++ apps/cron.py
@@ -5,7 +5,7 @@ class CronService(hass.Hass):
     """ Cron service to run scheduled tasks """
 
     def initialize(self, *args, **kwargs):
-        self.run_minutely(self.fetch_savings, datetime.now() , **kwargs)
+        self.run_every(self.fetch_savings, datetime.now(), 1, **kwargs)
 
     def fetch_savings(self, *args, **kwargs):
         self.log('I ran')
