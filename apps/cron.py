144babce30775b7acbd0c3e86d12e80e6b846aaa
Janos Molnar <janosmolnar1001@gmail.com>
2019-05-31 21:24:43 +0200

    WIP

--- apps/cron.py
+++ apps/cron.py
@@ -1,15 +1,14 @@
 from datetime import datetime
 import appdaemon.plugins.hass.hassapi as hass
-from juice.main import execute
+from juice import Juice
 
 class CronService(hass.Hass):
     """ Cron service to run scheduled tasks """
 
     def initialize(self, *args, **kwargs):
-        self.run_minute(self.fetch_savings, datetime.now(), 1, **kwargs)
+        self.run_every(self.fetch_savings, datetime.now(), 1, **kwargs)
 
     def fetch_savings(self, *args, **kwargs):
-        self.log('I ran')
-        execute()
+        Juice().execute()
 
 
