aa2d703bf46097df9ad258db031b694d588ce3d4
Janos Molnar <janosmolnar1001@gmail.com>
2019-05-31 21:01:45 +0200

    Add basic service

---
+++ apps/cron.py
@@ -0,0 +1,12 @@
+import appdaemon.plugins.hass.hassapi as hass
+
+class CronService(hass.Hass):
+    """ Cron service to run scheduled tasks """
+
+    def initialize(self, *args, **kwargs):
+        self.run_in(self.fetch_savings, 1, **kwargs)
+
+    def fetch_savings(self, *args, **kwargs):
+        self.log('I ran')
+
+
