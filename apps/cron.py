5926ad4e04806b3c3ae15e8d7e5468e85292e604
Janos Molnar <janosmolnar1001@gmail.com>
2019-05-31 21:41:41 +0200

    WIP

--- apps/cron.py
+++ apps/cron.py
@@ -6,7 +6,7 @@ class CronService(hass.Hass):
     """ Cron service to run scheduled tasks """
 
     def initialize(self, *args, **kwargs):
-        self.run_minutely(self.fetch_savings, datetime.datetime(2019, 05, 31, 21, 36), 1, **kwargs)
+        self.run_minutely(self.fetch_savings, datetime.datetime(2019, 05, 31, 21, 45), 1, **kwargs)
 
     def fetch_savings(self, *args, **kwargs):
         Juice().execute()
