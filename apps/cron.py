2bb3bd23c07f19de17ffba5679e8ee89ebd336dd
Janos Molnar <janosmolnar1001@gmail.com>
2019-05-31 21:14:36 +0200

    Use juice to fetch my savings

--- apps/cron.py
+++ apps/cron.py
@@ -1,13 +1,15 @@
 from datetime import datetime
 import appdaemon.plugins.hass.hassapi as hass
+from juice.main import execute
 
 class CronService(hass.Hass):
     """ Cron service to run scheduled tasks """
 
     def initialize(self, *args, **kwargs):
-        self.run_every(self.fetch_savings, datetime.now(), 1, **kwargs)
+        self.run_minute(self.fetch_savings, datetime.now(), 1, **kwargs)
 
     def fetch_savings(self, *args, **kwargs):
         self.log('I ran')
+        execute()
 
 
