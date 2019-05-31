cdf1891bcb1b0fd0d43cc952ddcf00bcdb228383
Janos Molnar <janosmolnar1001@gmail.com>
2019-05-31 21:05:33 +0200

    WIP

--- apps/cron.py
+++ apps/cron.py
@@ -1,10 +1,11 @@
+from datetime import datetime
 import appdaemon.plugins.hass.hassapi as hass
 
 class CronService(hass.Hass):
     """ Cron service to run scheduled tasks """
 
     def initialize(self, *args, **kwargs):
-        self.run_minutely(self.fetch_savings, 1, **kwargs)
+        self.run_minutely(self.fetch_savings, datetime.now() , **kwargs)
 
     def fetch_savings(self, *args, **kwargs):
         self.log('I ran')
